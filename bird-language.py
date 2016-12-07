a7959d648d0fb0e013479e09ef07ff6f54283417
shrumplestiltskin <shrumplestiltskin@gmail.com>
2016-12-06 21:16:28 -0600

    updating

--- bird-language.py
+++ bird-language.py
@@ -1,8 +1,20 @@
-VOWELS = "aeiouy"
-
 def translate(phrase):
-    for x in range(len(list(phrase))):
-        if phrase[x] not in VOWELS and phrase[x]+1 in VOWELS:
+    import re
+    consonant = re.compile('[^aeiouy]+[a|e|i|o|u|y]')
+    vowels = re.compile('[a]{3}|[e]{3}|[i]{3}|[o]{3}|[u]{3}|[y]{3}')
+    phrase = phrase.split()
+    for x in range(len(phrase)):
+        t = 0
+        for m in consonant.finditer(phrase[x]):
+            temp = list(phrase[x])
+            if m.start() == 0: phrase[x] = phrase[x][m.start()-t:m.end()-t].replace(m.group(), m.group()[0], 1) + phrase[x][m.end()-t:]
+            else: phrase[x] = phrase[x][0:m.start()-t] + phrase[x][m.start()-t:m.end()-t].replace(m.group(), m.group()[0], 1) + phrase[x][m.end()-t:]
+            t += 1
+    for x in range(len(phrase)):
+        for m in vowels.finditer(phrase[x]):
+            phrase[x] = phrase[x].replace(m.group(), m.group()[0], 1)
+    return ' '.join(phrase)
+
     
 if __name__ == '__main__':
     print(translate("hieeelalaooo"))
